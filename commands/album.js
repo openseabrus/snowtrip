6c33495a7a33041b112986b89e357a99a9649830
l1904l <g.seabra@protonmail.com>
2019-11-22 22:52:51 +0000

    Add await to delete Fetching Webcams message

--- commands/album.js
+++ commands/album.js
@@ -1,9 +1,10 @@
 const { ALBUM, webcams } = require('../constants');
 
-const album = (app) => app.command(ALBUM, ({ reply, replyWithMediaGroup }) => {
+const album = (app) => app.command(ALBUM, async ({ reply, replyWithMediaGroup, deleteMessage }) => {
   const now = Date.now();
 
-  reply('Fetching webcams...');
+  const fetchingId = await reply('Fetching webcams...');
+  const { message_id: messageId } = fetchingId;
 
   const pictures = Object.values(webcams)
     .filter((webcam) => !!webcam.type)
@@ -12,7 +13,8 @@ const album = (app) => app.command(ALBUM, ({ reply, replyWithMediaGroup }) => {
       media: `${webcam.media}${now}`,
     }));
 
-  replyWithMediaGroup(pictures);
+  await replyWithMediaGroup(pictures);
+  deleteMessage(messageId);
 });
 
 module.exports = album;
