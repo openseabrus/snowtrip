099e3ab7044699f9422efee126b5d7f00f3bf131
Rui Silva <rui.silva@diconium.com>
2020-01-23 10:19:25 +0000

    feat: adds stream for schmitten with a menu

--- commands/stream.js
+++ commands/stream.js
@@ -1,19 +1,22 @@
-const { keyboard } = require('telegraf/markup');
+const { Markup } = require('telegraf');
 const { STREAM } = require('../constants');
 
-const stream = (app) => {
-  app.hears('🇦🇹Schmitten stream', ({ reply }) => reply(
-    'https://s1.live-panorama.com/keblivestreaming/streams/schmittenTVLow.sdp.stream.m3u8',
-  ));
+const stream = (app) => app.command(STREAM, ({ reply }) => {
+  app.action('🇦🇹Schmitten stream', ({ editMessageText }) => {
+    editMessageText(STREAM,
+      reply('https://s1.live-panorama.com/keblivestreaming/streams/schmittenTVLow.sdp.stream.m3u8'));
+  });
 
-  app.hears('🇪🇸Sierra Nevada stream',
-    ({ reply }) => reply('https://stream.viddeon.com/borreguiles/borreguiles.m3u8'));
+  app.action('🇪🇸Sierra Nevada stream', ({ editMessageText }) => {
+    editMessageText(STREAM,
+      reply('https://stream.viddeon.com/borreguiles/borreguiles.m3u8'));
+  });
 
-  return app.command(STREAM, ({ reply }) => reply('One time keyboard',
-    keyboard(['🇪🇸Sierra Nevada stream', '🇦🇹Schmitten stream'])
-      .oneTime()
-      .resize()
-      .extra()));
-};
+  return reply(STREAM,
+    Markup.inlineKeyboard([
+      Markup.callbackButton('🇦🇹Schmitten stream', '🇦🇹Schmitten stream'),
+      Markup.callbackButton('🇪🇸Sierra Nevada stream', '🇪🇸Sierra Nevada stream'),
+    ]).extra());
+});
 
 module.exports = stream;
