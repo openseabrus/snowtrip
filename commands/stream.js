7c5b463ea1a5c48ebaf756a22426c40e236c2d4b
Rui Silva <rui.silva@diconium.com>
2020-01-23 09:26:40 +0000

    feat: adds stream for schmitten with a menu

--- commands/stream.js
+++ commands/stream.js
@@ -1,5 +1,19 @@
+const { keyboard } = require('telegraf/markup');
 const { STREAM } = require('../constants');
 
-const stream = (app) => app.command(STREAM, ({ reply }) => reply('https://stream.viddeon.com/borreguiles/borreguiles.m3u8'));
+const stream = (app) => {
+  app.hears('🇦🇹Schmitten stream', ({ reply }) => reply(
+    'https://s1.live-panorama.com/keblivestreaming/streams/schmittenTVLow.sdp.stream.m3u8',
+  ));
+
+  app.hears('🇪🇸Sierra Nevada stream',
+    ({ reply }) => reply('https://stream.viddeon.com/borreguiles/borreguiles.m3u8'));
+
+  return app.command(STREAM, ({ reply }) => reply('One time keyboard',
+    keyboard(['🇪🇸Sierra Nevada stream', '🇦🇹Schmitten stream'])
+      .oneTime()
+      .resize()
+      .extra()));
+};
 
 module.exports = stream;
